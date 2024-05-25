c930032ab5103b74b7fd44f6c03ebbdc09ab362e
timadigwe <timothyadigwe2019@gmail.com>
2024-05-25 21:47:33 +0100

    typescript updates

---
+++ web/src/app/components/LoadingIndicator.tsx
@@ -0,0 +1,50 @@
+import React, { CSSProperties, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import { makeStyles } from '@mui/styles';
+import { useEffectAfterTimeout } from '../utils/utils';
+import { Theme } from '@mui/material';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    padding: theme.spacing(2),
+  },
+}));
+
+interface LoadingIndicatorProps {
+  height?: string | number | null;
+  delay?: number;
+  [key: string]: unknown;
+}
+
+const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({
+  height = null,
+  delay = 500,
+  ...rest
+}) => {
+  const classes = useStyles();
+  const [visible, setVisible] = useState(false);
+
+  useEffectAfterTimeout(() => setVisible(true), delay);
+
+  const style: CSSProperties = {};
+  if (height) {
+    style.height = height;
+  }
+
+  if (!visible) {
+    return height ? <div style={style} /> : null;
+  }
+
+  return (
+    <div className={classes.root} style={style} {...rest}>
+      <CircularProgress />
+    </div>
+  );
+};
+
+export default LoadingIndicator;
